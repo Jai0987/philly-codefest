6d58f4507eaee7a00368d89916dedbbda96855d5
Shreyan Gupta <sg3778@drexel.edu>
2023-03-12 05:58:30 -0400

    Co-authored-by: Kashyap,Jainam <jk3862@drexel.edu>

--- Website/intro.js
+++ Website/intro.js
@@ -1,24 +1,23 @@
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml11 .letters')
+var textWrapper = document.querySelector('.sliding-text .letters')
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
 
 anime.timeline({ loop: false })
 	.add({
-		targets: '.ml11 .line',
+		targets: '.sliding-text .line',
 		scaleY: [0, 1],
 		opacity: [0.5, 1],
 		easing: 'easeOutExpo',
 		duration: 700,
 	})
 	.add({
-		targets: '.ml11 .line',
-		translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+		targets: '.sliding-text .line',
+		translateX: [0, document.querySelector('.sliding-text .letters').getBoundingClientRect().width + 10],
 		easing: 'easeOutExpo',
 		duration: 700,
 		delay: 100,
 	})
 	.add({
-		targets: '.ml11 .letter',
+		targets: '.sliding-text .letter',
 		opacity: [0, 1],
 		easing: 'easeOutExpo',
 		duration: 600,
@@ -31,3 +30,27 @@ anime.timeline({ loop: false })
 		duration: 1000,
 		easing: 'easeOutExpo',
 	})
+
+const canvas = document.querySelector('canvas')
+const renderer = new THREE.WebGLRenderer({ canvas })
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const scene = new THREE.Scene()
+
+// Add lights
+const light = new THREE.AmbientLight(0xffffff, 1)
+scene.add(light)
+
+const loader = new THREE.GLTFLoader()
+
+loader.load('scene.gltf', function (gltf) {
+	const model = gltf.scene
+	scene.add(model)
+	animate()
+})
+
+camera.position.z = 5
+
+function animate() {
+	requestAnimationFrame(animate)
+	renderer.render(scene, camera)
+}
